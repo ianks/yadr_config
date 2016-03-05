ac644b93a2e6b3fb0c9352e08ea183948fbc1ff2
Ian Ker-Seymer <i.kerseymer@gmail.com>
2016-03-04 19:02:40 -0500

    Update some things

--- dotfiles/.zsh.after/env.zsh
+++ dotfiles/.zsh.after/env.zsh
@@ -1,35 +1,18 @@
-platform=$(uname)
-
-if [[ $platform == 'Darwin' ]]; then
-  export DOCKER_HOST=tcp://192.168.59.103:2376
-  export DOCKER_CERT_PATH=/Users/ianks/.boot2docker/certs/boot2docker-vm
-  export DOCKER_TLS_VERIFY=1
-  export JAVA_HOME=$(/usr/libexec/java_home)
-fi
-
-if [[ $platform == 'Linux' ]]; then
-  # sudo
-  alias sudo='sudo'
-  alias nsudo='nocorrect sudo'
-
-  # Host
-  export HOST='0.0.0.0'
-fi
-
-# nodenv
-export PATH="$HOME/.nodenv/bin:$PATH"
-if which nodenv > /dev/null; then
-  eval "$(nodenv init -)"
-fi
-
 # Jruby
 export JRUBY_OPTS='--dev'
 
 # Other
 alias cdd='cd $HOME/Dropbox/Development'
 
+# Dircolors for termite
 [ "$TERM" = "xterm-termite" ] && eval $(dircolors $HOME/.dircolors)
 
+# Use nvim
 alias vim='nvim'
 
-export JRUBY_OPTS="--dev"
+# Tmuxifier
+export PATH="$HOME/.tmuxifier/bin:$PATH"
+source /usr/share/nvm/init-nvm.sh
+
+# FZF
+export FZF_DEFAULT_COMMAND='ag -g ""'
